40748b8a356ad12f40db3cbb02de2e429ab8ca72
vtduy3100 <vtduy3100@gmail.com>
2020-12-12 09:32:36 +0700

    class structure, project base structure

---
+++ app/src/main/java/com/example/onlinetherapist/Constant.java
@@ -0,0 +1,9 @@
+package com.example.onlinetherapist;
+
+public class Constant {
+    public static String PATIENT_TABLE = "Patients";
+    public static String APPOINTMENT_TABLE = "Appointment";
+    public static String TODOLIST_TABLE = "Todolist";
+    public static String TODOLISTITEM_TABLE = "Todolist_item";
+    public static String NOTE_TABLE = "Note";
+}
